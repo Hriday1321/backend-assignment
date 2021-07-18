0c3624aec2575a90e2a30b53a064eaa4cd1600e6
EC2 Default User <hridaygupta100@hotmail.com>
2021-07-18 16:37:19 +0000

    accomodated proper structure

--- app.js
+++ app.js
@@ -19,71 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.render("home.ejs");
-})
-
-app.get("/invoices", function(req, res){
-    Invoice.find({}, function(err, data){
-        if(err){
-            console.log(err)
-        }else {
-            res.render("invoices.ejs", {invoices: data});
-        }
-    })
-})
-
-app.post("/invoices", function(req, res){
-    var invoice = {to:req.body.to, from:req.body.from, notes:req.body.notes, due:req.body.due, status:req.body.stat, items: []};
-    for(var i=0; i < req.body.itemname.length; i++){
-        var item = {name: '', price: '', misc: ''};
-        item.name = req.body.itemname[i];
-        item.cost = req.body.cost[i];
-        item.misc = req.body.misc[i];
-        invoice.items = [...invoice.items,item];
-    }
-    Invoice.create(invoice, function(err, data){
-        if(err)
-            console.log(err);
-        else {
-                console.log("added invoice");
-            }
-        res.redirect("/invoices");
-    })
-})
-
-app.get("/invoices/new", function(req, res){
-    res.render("form.ejs");
-})
-
-app.get("/invoice/:id", function(req, res){
-    Invoice.findById(req.params.id, function(err, data){
-        if(err){
-            console.log(err)
-        } else{
-            res.send(data);
-        }
-    });
-})
-
-app.post("/invoice/:id", function(req, res){
-    Invoice.findById(req.params.id, function(err, item){
-        if(err){
-            console.log(err)
-        } else{
-            item.status = req.body.stat;
-            
-            item.save(function(err){
-                if(err){
-                    console.log(err)
-                }
-                else{
-                    res.redirect("/invoices")
-                }
-            });
-        }
-    });
-})
+app.use("/", require("./routes/api"))
 
 app.listen(process.env.PORT, process.env.IP, function(){
  console.log("server started");
